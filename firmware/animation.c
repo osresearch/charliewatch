e72f3abfd496a50c6e03599f978c904c2bc51e6c
Trammell hudson <hudson@trmm.net>
2018-10-15 21:23:20 -0400

    more animations for hand alignments

--- firmware/animation.c
+++ firmware/animation.c
@@ -1,6 +1,13 @@
 /*! \file animation.c
 
-  \brief Drawing functions based on the time
+  \brief Drawing functions based on the time.
+
+Animations are drawn:
+* When the second hand hits the minute hand
+* When the second hand hits the minute hand on a 5 minut emark
+* When the second hand hits the hour hand
+* When all three hands line up
+* At midnight/noon
 */
 
 #include <msp430.h>
@@ -41,12 +48,83 @@ void led_draw()
 }
 
 
+void sparkle_animation(unsigned count)
+{
+	// should make the sparkles converge inward
+	(void) count;
+	led_on(rand() % 60);
+	led_on(rand() % 60);
+	led_on(rand() % 60);
+	led_on(60 + (rand() % 12));
+}
+
+// animation for when all the hands align
+void align_animation(unsigned count)
+{
+	unsigned i;
+
+	led_on(RTCSEC);
+
+	if (count > 60)
+	{
+		// draw out from the minute up to the count
+		for(i=0 ; i < 120 - count ; i++)
+			led_on((RTCMIN + i) % 60);
+
+		// draw out from the hour hand in reverse
+		for(i=0 ; i < (120 - count)/5 ; i ++)
+			led_on(60 + (RTCHOUR + 12 - i) % 12);
+	} else {
+		// start collapsing back to the minute
+		for(i= 0 ; i < count ; i++)
+			led_on((RTCMIN - i + 60) % 60);
+
+		// collapse back to the hour hand
+		for(i=0 ; i < count/5 ; i ++)
+			led_on(60 + (RTCHOUR + i) % 12);
+	}
+}
+
+void hour_animation(unsigned count)
+{
+	led_display[2] = RTCSEC;
+
+	// run the hour forward
+	if (0 == (count & 3))
+		led_display[1] = 60 + (led_display[1] - 60 + 1 + 12) % 12;
+
+	led_draw();
+}
+
+
+void minute_animation(unsigned count)
+{
+	// run the minute hand backwards at half speed
+	if (0 == (count & 1))
+		led_display[0] = (led_display[0] + 60 - 1) % 60;
+
+	// run the second hand forward at full speed
+	led_display[2] = (led_display[2] + 1) % 60;
+
+	led_draw();
+}
+
+
+void second_animation(unsigned count)
+{
+	// run the second hand forward at full speed
+	led_display[2] = (led_display[2] + 1) % 60;
+
+	led_draw();
+}
+
 void animation_draw()
 {
 	static int oldsec;
 	static int do_second_animation = 0;
 	static int do_minute_animation = 0;
 	static int do_hour_animation = 0;
+	static int do_align_animation = 0;
 	static int do_sparkle_animation = 0;
 	static int hour_dir;
 	static unsigned hour_bright = 0;
@@ -65,56 +143,65 @@ void animation_draw()
 
 		const unsigned hour_five = 5 * (RTCHOUR % 12);
 
+		if (RTCMIN == 0 && RTCSEC == 0 && hour_five == 0)
+		{
+			// midnight or noon, draw the sparkles
+			do_sparkle_animation = 240;
+		} else
 		if (RTCMIN == RTCSEC && RTCMIN == hour_five)
 		{
 			// when the hour, minute and second hands line up
-			do_sparkle_animation = 240;
+			do_align_animation = 120;
 		} else
-		if (RTCSEC == 0 && (RTCMIN % 5) == 0)
+		if (RTCSEC == RTCMIN && (RTCMIN % 5) == 0)
 		{
+			// when the minute and second line up
 			// every five minutes
 			// wrap the second hand twice
 			// and the minute hand at half speed.
 			do_minute_animation = 120;
-			do_second_animation = 120;
 		} else
 		if (RTCMIN == RTCSEC)
 		{
 			// when the second hand hits the minute hand,
-			// animate the second hand for one revolution
+			// run the second hand forwards
 			do_second_animation = 60;
 		} else
 		if (RTCSEC == hour_five)
 		{
+			// second hand aligns with the hour hand
 			do_hour_animation = 12 * 4;
 		}
 	}
 
-	if (do_second_animation)
-	{
-		// run the second hand forwards at a fast speed
-		led_display[2] = (led_display[2] + 1) % 60;
-		do_second_animation--;
-	}
-	if (do_minute_animation)
-	{
-		// run the minute hand backwards at half speed
-		if (0 == (do_minute_animation & 1))
-			led_display[0] = (led_display[0] + 60 - 1) % 60;
-		do_minute_animation--;
-	}
-	if (do_hour_animation)
-	{
-		if (0 == (do_hour_animation & 3))
-			led_display[1] = 60 + (led_display[1] - 60 + 1 + 12) % 12;
-		do_hour_animation--;
-	}
 	if (do_sparkle_animation)
 	{
-		led_display[0] = rand() % 60;
-		led_display[1] = 60 + rand() % 12;
-		led_display[2] = rand() % 60;
-		do_sparkle_animation--;
+		sparkle_animation(do_sparkle_animation--);
+		return;
+	}
+
+	if (do_align_animation)
+	{
+		align_animation(do_align_animation--);
+		return;
+	}
+
+	if (do_hour_animation)
+	{
+		hour_animation(do_hour_animation--);
+		return;
+	}
+
+	if (do_minute_animation)
+	{
+		minute_animation(do_minute_animation--);
+		return;
+	}
+
+	if (do_second_animation)
+	{
+		second_animation(do_second_animation--);
+		return;
 	}
 
 	// make the hour "breath"
