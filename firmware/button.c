753100f5ed7a3a4dac68bc823fed4566fc3fef4e
Trammell hudson <hudson@trmm.net>
2018-11-03 22:16:43 -0400

    make button support v0.2 boards

--- firmware/button.c
+++ firmware/button.c
@@ -8,11 +8,14 @@ unsigned button_long;
 void button_init(void)
 {
 	// configure button as input with internal pullup
-	// (v0.1 is reusing the RX pin)
-	P1SEL &= ~(1 << 5);
-	P1DIR &= ~(1 << 5);
-	P1REN |=  (1 << 5);
-	P1OUT |=  (1 << 5);
+	// (v0.1 is reusing the RX pin, 1.5)
+//#define BUTTON 5
+	// (v0.2 has a separate button on 1.7)
+#define BUTTON 7
+	P1SEL &= ~(1 << BUTTON);
+	P1DIR &= ~(1 << BUTTON);
+	P1REN |=  (1 << BUTTON);
+	P1OUT |=  (1 << BUTTON);
 }
 
 
@@ -23,7 +26,7 @@ void button_update(void)
 	button_long = button_short = 0;
 
 	// if button is pressed, increment hold counter
-	if ((P1IN & (1 << 5)) == 0)
+	if ((P1IN & (1 << BUTTON)) == 0)
 	{
 		button_counter++;
 
@@ -39,7 +42,7 @@ void button_update(void)
 
 	// button has been released
 	// if held and released for the right amount of time
-	if (5 < button_counter && button_counter < 60)
+	if (3 < button_counter && button_counter < 60)
 		button_short = 1;
 
 	button_counter = 0;
